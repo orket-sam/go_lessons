8eaffdd7fb89028483bb3d4ab803a74a76125ea4
orket <developerorket@gmail.com>
2024-01-11 23:07:46 +0300

    tests'

--- async_lesson_1/main.go
+++ async_lesson_1/main.go
@@ -1,3 +1,24 @@
 package main
 
-var names []string = []string{""}
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func main() {
+
+	var user User
+
+	stringUser := `{"name":"orket","age":23,"i":5616}`
+
+	if err := json.Unmarshal([]byte(stringUser), &user); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(user.Name)
+
+}
